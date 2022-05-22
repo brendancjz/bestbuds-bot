9c34e0d3d7c75e6d581e663f1cd239ba61bb6189
brendancjz <brendancjz@gmail.com>
2022-05-22 17:12:54 +0800

    updating the message command

--- src/main/java/Command/HelpCommand.java
+++ src/main/java/Command/HelpCommand.java
@@ -39,7 +39,9 @@ public class HelpCommand extends Command {
         String help = "";
 
         help += "<b>Help</b>\n\n";
-        help += "<u>Where can I clarify or give feedback?</u>\n";
+        help += "<u>What do I do after following the set up steps?</u>\n";
+        help += "You're all set! Await a message from BestBuds Bot.\n\n";
+        help += "<u>Where can I clarify my doubts or give feedback?</u>\n";
         help += "Reach out to @brendanchia!\n\n\n";
         help += "<em>BestBuds Bot is in BETA. Do give feedback or suggestions to improve the bot. :)</em>";
 
