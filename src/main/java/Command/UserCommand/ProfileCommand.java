0aa647a5d7a4b90d1ddf0217e9983780dec5c275
brendancjz <brendancjz@gmail.com>
2022-05-27 18:22:02 +0800

    Big changes. Able to persist data for profile users

--- src/main/java/Command/UserCommand/ProfileCommand.java
+++ src/main/java/Command/UserCommand/ProfileCommand.java
@@ -111,7 +111,7 @@ public class ProfileCommand extends Command {
 
         deeds += "<b><u>Your BestBud Details:</u></b>\n\n";
         deeds += "<em>Name:</em> " + name + "\n";
-        deeds += "<em>Date of Birth:</em> <pre>" + dob + "</pre>\n";
+        deeds += "<em>Date of Birth:</em> " + dob + "\n";
         deeds += "<em>Code:</em> " + code + "\n";
         deeds += "<em>Description:</em> " + desc + "\n";
 
