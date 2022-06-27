732eca0c3b620a4bad8e44bd43a1f149547c0749
brendancjz <brendancjz@gmail.com>
2022-06-27 18:10:20 +0800

    update doc upload

--- src/main/java/TelegramBot/BestBudsBot.java
+++ src/main/java/TelegramBot/BestBudsBot.java
@@ -106,7 +106,7 @@ public class BestBudsBot extends TelegramLongPollingBot {
 //                for (HeaderElement elem : httpresponse.getEntity().getContentType().getElements()) {
 //                    System.out.println(elem.getName());
 //                }
-                System.out.println("InputStream size: " + inputStream.available());
+                System.out.println("InputStream size: " + inputStream.readAllBytes().length);
                 java.io.File file = new java.io.File(document.getFileName());
                 System.out.println("File Size before converting: " + file.length());
                 System.out.println("File Name: " + file.getName());
